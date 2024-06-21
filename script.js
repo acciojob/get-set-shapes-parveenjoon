8dda91b2c2e97cd7bbd2fbc0ab2cb0f52a9708ea
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-06-21 21:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-//complete this code
-class Rectangle {}
+// Class definitions
+class Rectangle {
+	constructor(width, height) {
+		this.width = width;
+		this.height = height;
+	}
 
-class Square extends Animal {}
+	getArea() {
+		return this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		super(side, side);
+	}
+
+	getPerimeter() {
+		return 4 * this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+// Test suite
+if (typeof describe === 'function') {
+	describe('My Test Suite', function() {
+	  it('calculates area and perimeter correctly - 1', function() {
+	    const square = new Square(7);
+	    chai.expect(square.getArea()).to.equal(49); // checks if the area is correctly calculated
+	    chai.expect(square.getPerimeter()).to.equal(28); // checks if the perimeter is correctly calculated
+	  })
+	});
+}
